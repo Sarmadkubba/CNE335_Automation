5d429f9a8321f1790d3a63355988b0a0acacb485
Sarmadkubba <83927120+Sarmadkubba@users.noreply.github.com>
2021-12-06 19:03:05 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 # This is the template code for the CNA337 Final Project
-#Sarmad Kubba
-# Tamplit base code by Zachary Rubin , zrubin@rtc.edu
-# totring TJ Dewey
-# with corpration Rick Sturza
-# CNA 335 Fall 2021
+# Zachary Rubin, zrubin@rtc.edu
+# CNE 335 Fall 2021
+# Sarmad Kubba
+# template base code by Zachary Rubin , zrubin@rtc.edu
+# tutoring TJ Dewey
+# with Cooperation Rick Sturza
 
+
+
+from Server import Server
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.11 by Sarmad")
+    print("Server Automator v0.11 by SarmadKubba")
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
     # TODO - Create a Server object
-    Ec2 = Server("13.58.111.80") 
-    Eco = Ec2.ping()
-    print(Eco)
+    Ec2 = Server("13.58.111.80")
     # TODO - Call Ping method and print the results
+
+    Echo = Ec2.ping()
+    print(Echo)
